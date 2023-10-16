87435b890e83d46cfc798ae94d8032126b9dccc0
mdfultz <129699601+mdfultz@users.noreply.github.com>
2023-10-16 01:05:41 +0000

    try2

--- sql/horror_movies.sql
+++ sql/horror_movies.sql
@@ -1,4 +1,6 @@
-SELECT id, name, imdb_rating FROM movies
+
+SELECT id AS Movie_ID, name AS Movie_Title, imdb_rating AS Rating
+FROM movies
 WHERE genre= 'horror' AND year <= 1985
 ORDER BY imdb_rating DESC
 LIMIT 3; 
